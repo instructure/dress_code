4c637c25dc8750fc1eeca44314eae7792dfa0cdb
Ahmad Amireh <ahmad@amireh.net>
2021-04-01 13:11:29 -0600

    inline the test fixtures for extractor spec
    this cleans up the spec helpers to use tempfiles instead, consequently
    you don't need to mount ./tmp anymore in the command earlier in the
    series:
    
        docker run --rm -it \
          -v "$PWD":/usr/src/app:ro \
          -w /usr/src/app \
          dkamer/canvas-lms-base@sha256:d55d85b8d84a9c1e0d57d865e49c562dcfb0b6be4eae10946722436386330ecb \
            sh -c 'bundle install && bundle exec rspec spec/extractor_spec.rb'

--- spec/extractor_spec.rb
+++ spec/extractor_spec.rb
@@ -1,33 +1,64 @@
 require_relative 'spec_helper'
 
 describe DressCode::Extractor do
+  let(:file) { Tempfile.new }
+  after(:each) { file.unlink }
+
   it "extracts documentation from files" do
-    @test_file = create_test_file
-    @extractor = DressCode::Extractor.new({
-      :files => [@test_file],
+    file.write <<~EOF
+      /* @styleguide button
+
+      ```html
+      <button>I am a button</button>
+      ```
+
+      */
+
+      button { background: OldLace; }
+
+      /* @styleguide anchor
+
+      ```html
+      <a>I am an anchor</a>
+      ```
+
+      */
+
+      a { background: BurlyWood; }
+    EOF
+
+    file.close
+
+    extractor = DressCode::Extractor.new({
+      :files => [file.path],
       :base_dir => 'tmp'
     })
 
-    docs = @extractor.extract
+    docs = extractor.extract
     docs.length.should == 2
     docs[0].component.should == 'anchor'
     docs[0].prose.should == "```html\n<a>I am an anchor</a>\n```"
-    docs[0].path.should == "tmp/test.css"
-    docs[0].relative_path.should == "test.css"
+    docs[0].path.should == file.path
     docs[1].component.should == 'button'
     docs[1].prose.should == "```html\n<button>I am a button</button>\n```"
-    docs[1].path.should == "tmp/test.css"
-    docs[1].relative_path.should == "test.css"
+    docs[1].path.should == file.path
   end
 
   it "doesn't choke on utf8 content" do
-    @test_file = create_utf8_test_file
-    @extractor = DressCode::Extractor.new({
-      :files => [@test_file],
+    file.write <<~EOF
+      button {
+        content: '▲';
+      }
+    EOF
+
+    file.close
+
+    extractor = DressCode::Extractor.new({
+      :files => [file.path],
       :base_dir => 'tmp'
     })
 
-    @extractor.extract
+    extractor.extract
   end
 end
 
